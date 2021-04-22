4b03c8f0dd19e2c8c9fa1d097e8a8b94c9b8a7d9
igalv <vilenskyigal@gmail.com>
2021-04-22 15:17:25 +0200

    js finally

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,19 @@
 /* const modeSwitch = document.querySelector(".switch input");
 console.log(modeSwitch); */
 
-const modeSwitch = document.querySelector(".slider");
+/* const modeSwitch = document.querySelector(".slider");
 modeSwitch.addEventListener("click", () => {
   document.body.classList.toggle("slider-open");
-});
+}); */
+
+const theme = () => {
+  const mood = document.querySelector("#light-dark").checked;
+  body = document.querySelector("body").style;
+  if (mood) {
+    body.backgroundColor = "white";
+  } else {
+    body.backgroundColor = "black";
+  }
+};
+
+/* const test = document.querySelector("body"); */
